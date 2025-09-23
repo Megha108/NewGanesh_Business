ddf3b350dc6da38eb78c6a52d58d7e4500262817
Megha-patel1203 <megha1203patel@gmail.com>
2025-09-23 14:48:57 +0530

    add all images in webp format

--- new-ganesh/src/common/navbar.jsx
+++ new-ganesh/src/common/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../assets/image/common/Logo.png";
+import Logo from "../assets/image/common/Logo.webp";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
