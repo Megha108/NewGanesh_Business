2797e54604cfda06cb8430e2470ffb73972db6c2
NityaCoding-gif <mynewview18@gmail.com>
2025-11-08 12:52:31 +0530

    commit
    -added model auto reset
    -refresh or page change to open the top of the page

--- new-ganesh/src/App.jsx
+++ new-ganesh/src/App.jsx
@@ -1,17 +1,28 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import HomeMain from "./pages/home/main";
 import AboutUsPage from "./pages/aboutUsPage/main.jsx";
-
 import ProductMain from "./pages/productpage/main.jsx";
 import SeedTrail from "./common/SeedTrail";
 import EnquiryPage from "./pages/enquiryPage/enquiryPage.jsx";
+import ScrollToTop from "./common/ScrollToTop.jsx"; // 👈 Import scroll helper
 
 export default function App() {
+  // 👇 Handle manual refresh scroll reset
+  useEffect(() => {
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+  }, []);
+
   return (
     <>
       {/* 🌱 Global floating animation */}
       <SeedTrail />
+
+      {/* 👇 Scroll to top on route change */}
+      <ScrollToTop />
 
       <Routes>
         <Route path="/" element={<HomeMain />} />
