d6d5ed84b61fac3526d450f95403bfb01119e9b4
NityaCoding-gif <mynewview18@gmail.com>
2025-11-13 11:01:31 +0530

    commit
    -photo size increased

--- new-ganesh/src/pages/home/OwnerPhoto.jsx
+++ new-ganesh/src/pages/home/OwnerPhoto.jsx
@@ -9,7 +9,7 @@ export default function OwnerPhoto() {
       
       {/* 🔹 Section 1 - Left Image, Right Text */}
       <section className="flex flex-col sm:flex-row items-center justify-start sm:justify-start sm:gap-[1px] p-[1px] sm:p-[1px] sm:pl-[5%]">
-        <div className="w-[260px] h-[250px] overflow-hidden rounded-md">
+        <div className="w-[260px] h-[250px] overflow-hidden rounded-md scale-[1.3]">
           <img
             src={ownerImg}
             alt="Owner"
@@ -33,7 +33,7 @@ export default function OwnerPhoto() {
 
       {/* 🔹 Section 2 - Right Image, Left Text but RIGHT-ALIGNED INFO */}
       <section className="flex flex-col sm:flex-row-reverse items-center justify-start sm:justify-start gap-[1px] sm:gap-[1px] p-[1px] sm:p-[1px] sm:pr-[5%]">
-        <div className="w-[260px] h-[250px] overflow-hidden rounded-md">
+        <div className="w-[260px] h-[250px] overflow-hidden rounded-md scale-[1.3]">
           <img
             src={ownerImg2}
             alt="Owner 2"
@@ -57,7 +57,7 @@ export default function OwnerPhoto() {
 
       {/* 🔹 Section 3 - Left Image, Right Text */}
       <section className="flex flex-col sm:flex-row items-center justify-start sm:justify-start gap-[1px] sm:gap-[1px] p-[1px] sm:p-[1px] sm:pl-[5%]">
-        <div className="w-[260px] h-[250px] overflow-hidden rounded-md">
+        <div className="w-[260px] h-[250px] overflow-hidden rounded-md scale-[1.3]">
           <img
             src={ownerImg3}
             alt="Owner 3"
