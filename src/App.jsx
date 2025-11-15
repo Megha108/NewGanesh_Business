8503ee72db992f96eb918b8c4b1c7ea729801e16
NityaCoding-gif <121719987+Megha108@users.noreply.github.com>
2025-11-15 10:56:48 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ProductMain from "./pages/productpage/main.jsx";
 import SeedTrail from "./common/SeedTrail";
 import EnquiryPage from "./pages/enquiryPage/enquiryPage.jsx";
 import GalleryPage from "./pages/gallerypage/GalleryPage.jsx";
+
 import ScrollToTop from "./common/ScrollToTop.jsx"; // 👈 Import scroll helper
 
 export default function App() {
