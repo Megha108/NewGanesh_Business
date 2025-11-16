8e08aa84d1352b2feb94e67930d353086068ee74
Manavpatel99 <112459650+Manavpatel99@users.noreply.github.com>
2025-11-16 12:01:43 +0530

    update port in server.js

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ Submitted On: ${new Date().toLocaleString()}
 // =========================
 //  SERVER START (IMPORTANT)
 // =========================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
